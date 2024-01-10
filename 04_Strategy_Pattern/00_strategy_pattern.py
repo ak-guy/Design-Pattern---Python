4d7c1318039a7456684144b59d26acff183ad067
Arpit <arpitkumar3203@gmail.com>
2024-01-10 10:01:44 +0530

    added strategy pattern

---
+++ 04_Strategy_Pattern/00_strategy_pattern.py
@@ -0,0 +1,4 @@
+'''
+The strategy pattern defined a family of algorithms, it encapsulates each one and makes them interchangeable.
+Strategy lets the algorithm vary independently from client that use it.
+'''
